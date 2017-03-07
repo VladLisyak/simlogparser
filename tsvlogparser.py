aaf9244ee5ed9657d5b5674a7668ecdc087cfdda
Vladyslav Lysiak <vladyslav_lysiak@epam.com>
2017-03-07 18:06:39 +0200

    Changed json body, added arguments

--- tsvlogparser.py
+++ tsvlogparser.py
@@ -4,7 +4,6 @@
 import argparse
 import csv
 import re
-from difflib import SequenceMatcher
 from json import loads
 
 from influxdb import InfluxDBClient
@@ -15,29 +14,33 @@ JSON_BODY = '[{"measurement": "errors",' \
             '"tags": {"test_type": "capacity",' \
             '"simulation": "%(simulation)s",' \
             '"user_count": 20,' \
-            '"request_name": "%(request_name)s",' \
+            '"request_name": "%(request_name)s"},'\
+            '"time": %(request_start)s,' \
+            '"fields": {"response_time": %(response_time)s,' \
             '"error_status": "%(response_code)s",' \
             '"error_code": "%(error_code)s",' \
-            '"error_details": "%(error)s"},' \
-            '"time": %(request_start)s,' \
-            '"fields": {"response_time": %(response_time)s}}]'
+            '"error_details": "%(error)s"}}]'
+
 SEEN_ERRORS = []
 
 
 class SimulationLogParser(object):
     def write_to_db(self, json_body):
+        """Write to influxdb"""
         client = InfluxDBClient('localhost', 8086, 'root', 'root', 'test')
         client.create_database('test')
         client.write_points(json_body)
 
     def parse_log(self, path):
+        """Entry point for simulation log parsing"""
         with open(path) as tsv:
             for line in csv.reader(tsv, delimiter="\t"):
                 if len(line) >= 8 and (line[7] == "KO"):
-                    data = self.parse(line)
+                    data = self.parseEntry(line)
                     self.write_to_db(loads(JSON_BODY % data))
 
-    def parse(self, values):
+    def parseEntry(self, values):
+        """Parse error entry"""
         regexp = re.match(r".+ (https?://.+?),* .*HTTP Code: (.+?), Response: ?(.*),*", values[9])
         error_code_regex = re.search(r'"code":"?(-?\d+)"?,', values[9])
 
@@ -51,7 +54,7 @@ class SimulationLogParser(object):
         arguments['gatling_error'] = values[8]
         arguments['response_code'] = self.extract_response_code(regexp.group(2))
         arguments['error_code'] = self.extract_error_code(error_code_regex)
-        arguments['error'] = values[9].replace('"', '\\"') #self.compare_error(current_err)
+        arguments['error'] = values[9].replace('"', '\\"')
         return arguments
 
     def extract_error_code(self, error_code_regex):
@@ -66,30 +69,27 @@ class SimulationLogParser(object):
             return code_matcher.group(1)
         return code
 
-    # def compare_error(self, current_err):
-    #     if len(SEEN_ERRORS) == 0:
-    #         SEEN_ERRORS.append(current_err)
-    #         return current_err
-    #     else:
-    #         for seen_err in SEEN_ERRORS:
-    #             if SequenceMatcher(None, current_err, seen_err).ratio() >= 0.7:
-    #                 return seen_err
-    #
-    #         SEEN_ERRORS.append(current_err)
-    #         return current_err
-
-
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Simlog parser.')
     parser.add_argument("-f", "--file", required=True, help="Log to parse.")
     # parser.add_argument("-c", "--count", required=True, type=int, help="User count.")
     # parser.add_argument("-t", "--type", required=True, help="Test type.")
+    # parser.add_argument("-s", "--host", required=True)
+    # parser.add_argument("-p", "--port")
+    # parser.add_argument("-l", "--login")
+    # parser.add_argument("-w", "--password")
+    # parser.add_argument("-b", "--database")
 
     args = vars(parser.parse_args())
 
     logPath = args['file']
     # userCount = args['count']
     # testType = args['type']
+    # host = args['host']
+    # port = args['port']
+    # login = args['login']
+    # password = args['password']
+    # db  = args['database']
 
     logParser = SimulationLogParser()
     logParser.parse_log(logPath)
